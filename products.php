77bfd39592e5df35b6255cd4057f8b4a43600dea
Zeeshan Web <Zeeshan Web>
2022-02-20 00:31:44 +0500

    Update dynamic product style and Fixed Price Calculation Issues

--- products.php
+++ products.php
@@ -21,7 +21,11 @@ function get_products_html_content() {
 
 				$product_name = $product_data['product_name'];
 				$product_image = $product_data['product_image'];
+				$product_currency = $product_data['product_currency'];
+				$product_price = $product_data['product_price'];
+				$product_discount = $product_data['product_discount'];
 
+				$selling_price = $product_price - ( $product_price * ( $product_discount / 100) );
 				?>
 				<a href="#" class="drz-product-link">
 			    	<div class="drz-product-box">
@@ -30,15 +34,15 @@ function get_products_html_content() {
 			    		</div>
 			    		<div class="drz-product-title"><?php echo $product_name; ?></div>
 			    		<div class="drz-product-prices">
-			    			<span class="drz-currency">Rs.</span>
-			    			<span class="drz-price">197</span>
+			    			<span class="drz-currency"><?php echo $product_currency; ?></span>
+			    			<span class="drz-price"><?php echo $selling_price; ?></span>
 			    		</div>
 			    		<div class="drz-product-origin-prices">
 			    			<span class="cross-mark">
-			    				<span class="drz-currency">Rs.</span>
-			    				<span class="drz-price">599</span>
+			    				<span class="drz-currency"><?php echo $product_currency; ?></span>
+			    				<span class="drz-price"><?php echo $product_price; ?></span>
 			    			</span>
-			    			<span class="drz-discount">-70%</span>
+			    			<span class="drz-discount"><?php echo '-'.$product_discount; ?></span>
 			    		</div>
 			    	</div>
 			    </a>
@@ -65,43 +69,88 @@ function get_electronics_product_data() {
 	$products_list = [ 
 		101		=>	[ 
 			'product_name' 		=> 'Flash Drive OTG',
-			'product_image' 	=> DRZ_ELECTRONICS_URL.'flash-drive-otg.jpg'
+			'product_image' 	=> DRZ_ELECTRONICS_URL.'flash-drive-otg.jpg',
+			'product_currency'	=> 'RS.',
+			'product_price'		=> 557,
+			'product_discount'	=> 50
+
 		],
 		102		=> 	[ 
 			'product_name' 		=> 'Head Phone',
-			'product_image' 	=> DRZ_ELECTRONICS_URL.'headphone.jpg'
+			'product_image' 	=> DRZ_ELECTRONICS_URL.'headphone.jpg',
+			'product_currency'	=> 'RS.',
+			'product_price'		=> 1000,
+			'product_discount'	=> 70
 		],
 		103		=> 	[ 
 			'product_name' 		=> 'Power Bank',
-			'product_image' 	=> DRZ_ELECTRONICS_URL.'powerbank.jpg'
+			'product_image' 	=> DRZ_ELECTRONICS_URL.'powerbank.jpg',
+			'product_currency'	=> 'RS.',
+			'product_price'		=> 800,
+			'product_discount'	=> 70
 		],
 		104		=> 	[ 
 			'product_name' 		=> 'Smart Watch',
-			'product_image' 	=> DRZ_ELECTRONICS_URL.'smart-watch.jpg'
+			'product_image' 	=> DRZ_ELECTRONICS_URL.'smart-watch.jpg',
+			'product_currency'	=> 'RS.',
+			'product_price'		=> 300,
+			'product_discount'	=> 70
 		],
 		105		=> 	[ 
 			'product_name' 		=> 'USB Charger',
-			'product_image' 	=> DRZ_ELECTRONICS_URL.'usb-charger.jpg'
+			'product_image' 	=> DRZ_ELECTRONICS_URL.'usb-charger.jpg',
+			'product_currency'	=> 'RS.',
+			'product_price'		=> 200,
+			'product_discount'	=> 70
 		],
 		106		=> 	[ 
 			'product_name' 		=> 'USB',
-			'product_image' 	=> DRZ_ELECTRONICS_URL.'usb.jpg'
+			'product_image' 	=> DRZ_ELECTRONICS_URL.'usb.jpg',
+			'product_currency'	=> 'RS.',
+			'product_price'		=> 600,
+			'product_discount'	=> 70
 		],
 		107		=> 	[ 
-			'product_name' 		=> 'USB',
-			'product_image' 	=> DRZ_ELECTRONICS_URL.'flash-drive-otg.jpg'
+			'product_name' 		=> 'Fast Brush',
+			'product_image' 	=> DRZ_ELECTRONICS_URL.'fast-brush.jpg',
+			'product_currency'	=> 'RS.',
+			'product_price'		=> 1200,
+			'product_discount'	=> 70
 		],
 		108		=> 	[ 
-			'product_name' 		=> 'USB',
-			'product_image' 	=> DRZ_ELECTRONICS_URL.'flash-drive-otg.jpg'
+			'product_name' 		=> 'Mini Vaccum',
+			'product_image' 	=> DRZ_ELECTRONICS_URL.'mini-vaccum.jpg',
+			'product_currency'	=> 'RS.',
+			'product_price'		=> 1800,
+			'product_discount'	=> 70
 		],
 		109		=> 	[ 
-			'product_name' 		=> 'USB',
-			'product_image' 	=> DRZ_ELECTRONICS_URL.'flash-drive-otg.jpg'
+			'product_name' 		=> 'Washing Machine',
+			'product_image' 	=> DRZ_ELECTRONICS_URL.'washing-machine.jpg',
+			'product_currency'	=> 'RS.',
+			'product_price'		=> 2000,
+			'product_discount'	=> 70
 		],
 		110		=> 	[ 
-			'product_name' 		=> 'USB',
-			'product_image' 	=> DRZ_ELECTRONICS_URL.'flash-drive-otg.jpg'
+			'product_name' 		=> 'Weight Machine',
+			'product_image' 	=> DRZ_ELECTRONICS_URL.'weight-machine.jpg',
+			'product_currency'	=> 'RS.',
+			'product_price'		=> 3000,
+			'product_discount'	=> 70
+		],
+		111		=> 	[ 
+			'product_name' 		=> 'Mouse',
+			'product_image' 	=> DRZ_ELECTRONICS_URL.'mouse.jpg',
+			'product_currency'	=> 'RS.',
+			'product_price'		=> 500,
+			'product_discount'	=> 70
+		],
+		112		=> 	[ 
+			'product_name' 		=> 'Sealine Strip',
+			'product_image' 	=> DRZ_ELECTRONICS_URL.'sealing-strip.jpg',
+			'product_currency'	=> 'RS.',
+			'product_price'		=> 750,
+			'product_discount'	=> 70
 		]
 	];
 
